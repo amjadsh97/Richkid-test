b5b44dcdffe0050ee9672e314ac76ed6266bfcc4
amjad <amjadshadid51@gmail.com>
2022-12-09 20:32:40 +0200

    Add home page

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const scrollButton = document.querySelector(".scroll-button");
+const menuWrapper = document.querySelector(".menu-wrapper");
+const closeMenuButton = document.querySelector("#close-icon");
+const openMenuButton = document.querySelector("#menu-button");
+
+scrollButton.addEventListener("click",() => {
+	window.scrollTo({
+		top:document.querySelector(".story-section")?.offsetTop,
+		behavior:"smooth",
+	})
+})
+
+openMenuButton.addEventListener("click", () => {
+	document.querySelector(':root').style.setProperty('--display-menu', `block`);
+	menuWrapper.classList.add("slideIn");
+	menuWrapper.classList.remove("slideOut");
+})
+
+closeMenuButton.addEventListener("click", () => {
+	menuWrapper.classList.add("slideOut");
+	document.querySelector(':root').style.setProperty('--display-menu', `block`);
+	menuWrapper.classList.remove("slideIn");
+})
